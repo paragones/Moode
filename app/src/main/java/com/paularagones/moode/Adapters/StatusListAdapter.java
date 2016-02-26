7d0d9149d197c610d854383eaa6e9fa51af192f5
Bobby <p.aragones@gmail.com>
2016-02-26 22:18:31 +0800

    checking out models

---
+++ app/src/main/java/com/paularagones/moode/Adapters/StatusListAdapter.java
@@ -0,0 +1,9 @@
+package com.paularagones.moode.Adapters;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Paul.Aragones on 2/26/2016.
+ */
+public class StatusListAdapter extends ArrayAdapter<> {
+}
---
+++ app/src/main/java/com/paularagones/moode/Models/Status.java
@@ -0,0 +1,8 @@
+package com.paularagones.moode.Models;
+
+/**
+ * Created by Paul.Aragones on 2/26/2016.
+ */
+public class Status {
+    
+}
