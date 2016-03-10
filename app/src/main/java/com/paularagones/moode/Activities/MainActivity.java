cb30f778a7cb02465b2cca2bacc480cf49121a5b
Bobby <p.aragones@gmail.com>
2016-03-10 13:36:46 +0800

    db merge part 6

--- app/src/main/java/com/paularagones/moode/Activities/MainActivity.java
+++ app/src/main/java/com/paularagones/moode/Activities/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        System.out.println("Yo ");
+        System.out.println("Yo Anj");
 
         setContentView(R.layout.main);
 
